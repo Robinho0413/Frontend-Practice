f26f3d8613ab769fcd9a7595edcf65ace5a09b02
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2024-11-26 11:55:10 +0100

    Add PP Monument Extended font families and update homepage layout for improved aesthetics

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,26 @@ import Button from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="bg-tile bg-repeat">
-      <div className="relative h-screen bg-hero bg-no-repeat bg-cover bg-center flex flex-row items-center justify-center gap-32 mask-gradient">
-        <div className="absolute inset-0 bg-black/65 backdrop-blur-sm"></div>
-        <div className="relative z-10 flex items-center justify-center gap-32">
-          <div>
-            <Image src="/images/cover.webp" alt="Cover" width={500} height={500} />
+      <div className="absolute inset-0 h-screen bg-hero bg-no-repeat bg-cover bg-center mask-gradient brightness-50 blur-sm"></div>
+      <div className="relative flex items-center justify-center gap-20 h-screen mt-16">
+        <div className="relative">
+          <Image src="/images/cover.webp" alt="Cover" width={500} height={500} />
+          <div className="absolute -left-2 bottom-0 rotate-[-90deg] origin-bottom-left text-white text-xl font-normal italic">
+            <span className="text-primary font-medium">Instinct</span> — Released May 22, 2020
           </div>
-          <div className="uppercase font-monextBlack">
-            <h1 className="text-3xl tracking-widest font-stroke">Level days</h1>
-            <h2 className="text-2xl">Conro</h2>
-            <div className="flex flex-row gap-2 mt-4">
-              <Button>
-                <Image src="/icons/play.svg" alt="Play" width={20} height={20} className="mr-2" />
-                Listen now
-              </Button>
-              <Button variant="secondary">
-                <Image src="/icons/share.svg" alt="Play" width={20} height={20} className="mr-2" />
-                Share
-              </Button>
-            </div>
+        </div>
+        <div className="uppercase">
+          <h1 className="text-5xl font-monextBlack tracking-widest font-stroke mb-3">Level days</h1>
+          <h2 className="text-3xl font-monextRegular font-bold mb-16">Conro</h2>
+          <div className="flex flex-row gap-2 mt-4">
+            <Button>
+              <Image src="/icons/play.svg" alt="Play" width={20} height={20} className="mr-2" />
+              Listen now
+            </Button>
+            <Button variant="secondary">
+              <Image src="/icons/share.svg" alt="Play" width={20} height={20} className="mr-2" />
+              Share
+            </Button>
           </div>
         </div>
       </div>
